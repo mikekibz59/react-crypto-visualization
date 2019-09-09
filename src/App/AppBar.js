44bc805a35321877f63d5ea7220c2d215ba3162c
mikekibz59 <mikekibz59@gmail.com>
2019-09-09 08:15:46 +0300

    added styling to the appbar

--- src/App/AppBar.js
+++ src/App/AppBar.js
@@ -1,18 +1,39 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Bar = styled.div`
   display: grid;
   grid-template-columns: 180px auto 100px 100px;
 `;
+const Logo = styled.div`
+  font-size: 1.5em;
+`;
+
+const ControlButtonElem = styled.div`
+  cursor: pointer;
+  ${props =>
+    props.active &&
+    css`
+      text-shadow: 0px 0px 60px #03ff03;
+    `}
+`;
+function toProperCase(lower) {
+  return lower.charAt(0).toUpperCase() + lower.substr(1);
+}
+
+function ControlButton({ name, active }) {
+  return (
+    <ControlButtonElem active={active}>{toProperCase(name)}</ControlButtonElem>
+  );
+}
 
 export default function() {
   return (
     <Bar>
-      <div> Cryptodash</div>
+      <Logo> Cryptodash</Logo>
       <div />
-      <div> Dashboard</div>
-      <div> Settings</div>
+      <ControlButton active name='dashboard' />
+      <ControlButton name='settings' />
     </Bar>
   );
 }
