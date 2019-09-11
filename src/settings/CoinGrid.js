6cc103af6541df35d1c65de1b80a8150f258e5de
mikekibz59 <mikekibz59@gmail.com>
2019-09-11 10:49:31 +0300

    Added coin grid

---
+++ src/settings/CoinGrid.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { AppContext } from '../App/AppProvider';
+
+export const CoinGridStyled = styled.div`
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+`;
+
+export default function() {
+  return (
+    <AppContext.Consumer>
+      {({ coinlist }) => (
+        <CoinGridStyled>
+          {Object.keys(coinlist).map(coinKey => (
+            <div>{coinKey}</div>
+          ))}
+        </CoinGridStyled>
+      )}
+    </AppContext.Consumer>
+  );
+}
