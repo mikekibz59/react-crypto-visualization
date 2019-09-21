c94164c90793bbadab2190eb9054d80c45fec897
mikekibz59 <mikekibz59@gmail.com>
2019-09-21 16:51:21 +0300

    added highchart and fixed a few warnings

--- src/settings/ConfirmButton.js
+++ src/settings/ConfirmButton.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { AppContext } from '../App/AppProvider';
-import { frontSize1, greenBoxShadow, color3 } from '../shared/styles';
+import { greenBoxShadow, color3 } from '../shared/styles';
 
 const ConfirmButtonStyled = styled.div`
-  margin: 20px;
-  color: ${color3};
-  padding: 5px;
-  cursor: pointer;
-  &:hover {
-    ${greenBoxShadow}
-  }
+	margin: 20px;
+	color: ${color3};
+	padding: 5px;
+	cursor: pointer;
+	&:hover {
+		${greenBoxShadow}
+	}
 `;
 
 export const CenterDiv = styled.div`
-  display: grid;
-  justify-content: center;
+	display: grid;
+	justify-content: center;
 `;
 
 export default function() {
-  return (
-    <AppContext.Consumer>
-      {({ confirmFavorites }) => (
-        <CenterDiv>
-          <ConfirmButtonStyled onClick={confirmFavorites}>
-            Confirm Favorites
-          </ConfirmButtonStyled>
-        </CenterDiv>
-      )}
-    </AppContext.Consumer>
-  );
+	return (
+		<AppContext.Consumer>
+			{({ confirmFavorites }) => (
+				<CenterDiv>
+					<ConfirmButtonStyled onClick={confirmFavorites}>
+						Confirm Favorites
+					</ConfirmButtonStyled>
+				</CenterDiv>
+			)}
+		</AppContext.Consumer>
+	);
 }
