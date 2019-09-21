b7e0acbe1f8981b51d072ea536cd41926787a631
mikekibz59 <mikekibz59@gmail.com>
2019-09-21 13:56:54 +0300

    Added coin spotlight

--- src/App/AppProvider.js
+++ src/App/AppProvider.js
@@ -7,103 +7,121 @@ export const AppContext = React.createContext();
 const MAX_FAVORITES = 10;
 
 export class AppProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      page: 'dashboard',
-      favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
-      ...this.savedSettings(),
-      setPage: this.setPage,
-      addCoin: this.addCoin,
-      isInFavorites: this.isInFavorites,
-      removeCoin: this.removeCoin,
-      confirmFavorites: this.confirmFavorites,
-      setFilteredCoins: this.setFilteredCoins
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			page: 'dashboard',
+			favorites: ['BTC', 'ETH', 'XMR', 'DOGE'],
+			...this.savedSettings(),
+			setPage: this.setPage,
+			addCoin: this.addCoin,
+			isInFavorites: this.isInFavorites,
+			removeCoin: this.removeCoin,
+			confirmFavorites: this.confirmFavorites,
+			setFilteredCoins: this.setFilteredCoins,
+			setCurrentFavorite: this.setCurrentFavorite,
+		};
+	}
 
-  //a function to fetch all coins and prices
-  componentDidMount() {
-    this.fetchCoins();
-    this.fetchPrices();
-  }
+	//a function to fetch all coins and prices
+	componentDidMount() {
+		this.fetchCoins();
+		this.fetchPrices();
+	}
 
-  fetchCoins = async () => {
-    let coinlist = (await cc.coinList()).Data;
-    this.setState({ coinlist });
-  };
+	fetchCoins = async () => {
+		let coinlist = (await cc.coinList()).Data;
+		this.setState({ coinlist });
+	};
 
-  addCoin = key => {
-    let favorites = [...this.state.favorites];
-    if (favorites.length < MAX_FAVORITES) {
-      if (!favorites.includes(key)) favorites.push(key);
-      this.setState({ favorites });
-    }
-  };
+	addCoin = (key) => {
+		let favorites = [...this.state.favorites];
+		if (favorites.length < MAX_FAVORITES) {
+			if (!favorites.includes(key)) favorites.push(key);
+			this.setState({ favorites });
+		}
+	};
 
-  removeCoin = key => {
-    let favorites = [...this.state.favorites].filter(favorite => {
-      return favorite !== key;
-    });
-    this.setState({ favorites });
-  };
+	removeCoin = (key) => {
+		let favorites = [...this.state.favorites].filter((favorite) => {
+			return favorite !== key;
+		});
+		this.setState({ favorites });
+	};
 
-  isInFavorites = key => _.includes(this.state.favorites, key);
+	setCurrentFavorite = (sym) => {
+		this.setState({ currentFavorite: sym });
+		localStorage.setItem(
+			'cryptoDash',
+			JSON.stringify({
+				...JSON.parse(localStorage.getItem('cryptoDash')),
+				currentFavorite: sym,
+			})
+		);
+	};
 
-  setPage = page => this.setState({ page });
+	isInFavorites = (key) => _.includes(this.state.favorites, key);
 
-  setFilteredCoins = filteredCoins => this.setState({ filteredCoins });
+	setPage = (page) => this.setState({ page });
 
-  savedSettings() {
-    let cryptoData = JSON.parse(localStorage.getItem('cryptoDash'));
-    if (!cryptoData) {
-      return { page: 'dashboard', firstVisit: true };
-    }
-    let { favorites } = cryptoData;
-    return { favorites };
-  }
+	setFilteredCoins = (filteredCoins) => this.setState({ filteredCoins });
 
-  fetchPrices = async () => {
-    if (this.state.firstVisit) return;
-    let prices = await this.prices();
-    console.log(prices);
-    prices = prices.filter(price => Object.keys(price).length);
-    this.setState({ prices });
-  };
+	savedSettings() {
+		let cryptoData = JSON.parse(localStorage.getItem('cryptoDash'));
+		if (!cryptoData) {
+			return { page: 'dashboard', firstVisit: true };
+		}
+		let { favorites, currentFavorite } = cryptoData;
+		return { favorites, currentFavorite };
+	}
 
-  prices = async () => {
-    let returnData = [];
-    for (let i = 0; i < this.state.favorites.length; i++) {
-      try {
-        let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
-        returnData.push(priceData);
-      } catch (e) {
-        console.warn('fetch price error: ', e);
-      }
-    }
-    return returnData;
-  };
-  confirmFavorites = () => {
-    this.setState(
-      {
-        firstVisit: false,
-        page: 'dashboard'
-      },
-      () => {
-        this.fetchPrices();
-      }
-    );
-    localStorage.setItem(
-      'cryptoDash',
-      JSON.stringify({ favorites: this.state.favorites })
-    );
-  };
+	fetchPrices = async () => {
+		if (this.state.firstVisit) return;
+		let prices = await this.prices();
+		console.log(prices);
+		prices = prices.filter((price) => Object.keys(price).length);
+		this.setState({ prices });
+	};
 
-  render() {
-    return (
-      <AppContext.Provider value={this.state}>
-        {this.props.children}
-      </AppContext.Provider>
-    );
-  }
+	prices = async () => {
+		let returnData = [];
+		for (let i = 0; i < this.state.favorites.length; i++) {
+			try {
+				let priceData = await cc.priceFull(this.state.favorites[i], 'USD');
+				returnData.push(priceData);
+			} catch (e) {
+				console.warn('fetch price error: ', e);
+			}
+		}
+		return returnData;
+	};
+
+	confirmFavorites = () => {
+		let currentFavorite = this.state.favorites[0];
+		this.setState(
+			{
+				firstVisit: false,
+				page: 'dashboard',
+				currentFavorite: currentFavorite,
+			},
+			() => {
+				this.fetchPrices();
+			}
+		);
+		localStorage.setItem(
+			'cryptoDash',
+			JSON.stringify({
+				favorites: this.state.favorites,
+				currentFavorite: currentFavorite,
+			})
+		);
+	};
+
+	render() {
+		return (
+			<AppContext.Provider value={this.state}>
+				{this.props.children}
+			</AppContext.Provider>
+		);
+	}
 }
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import styled from 'styled-components';
 import Page from '../shared/Page';
 import PriceGrid from './PriceGrid';
+import CoinSpotlight from './CoinSpotlight';
+
+const ChartGrid = styled.div`
+	display: grid;
+	margin-top: 20px;
+	grid-gap: 15px;
+	grid-template-columns: 1fr 3fr;
+`;
 
 export default function() {
 	return (
 		<Page name='dashboard'>
-			<PriceGrid></PriceGrid>
+			<PriceGrid />
+			<ChartGrid>
+				<CoinSpotlight />
+				<div>chart comes here</div>
+			</ChartGrid>
 		</Page>
 	);
 }
