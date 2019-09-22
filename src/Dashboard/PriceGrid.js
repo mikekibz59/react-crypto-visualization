34ead50341e0af00dca40bb3fb86568c8f34e710
mikekibz59 <mikekibz59@gmail.com>
2019-09-22 15:10:27 +0300

    fix critical bug

--- src/Dashboard/PriceGrid.js
+++ src/Dashboard/PriceGrid.js
@@ -4,25 +4,25 @@ import { AppContext } from '../App/AppProvider';
 import PriceTile from './PriceTile';
 
 const PriceGrid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(5, 1fr);
-  grid-gap: 15px;
-  margin-top: 40px;
+	display: grid;
+	grid-template-columns: repeat(5, 1fr);
+	grid-gap: 15px;
+	margin-top: 40px;
 `;
 
 export default function() {
-  return (
-    <AppContext.Consumer>
-      {({ prices }) => (
-        <PriceGrid key={prices}>
-          {prices.map((price, index) => (
-            <PriceTile price={price} index={index}>
-              {' '}
-              {Object.keys(price)[0]}
-            </PriceTile>
-          ))} 
-          </PriceGrid>        
-      )}
-    </AppContext.Consumer>
-  );
+	return (
+		<AppContext.Consumer>
+			{({ prices }) => (
+				<PriceGrid key={prices}>
+					{prices.map((price, index) => (
+						<PriceTile price={price} index={index}>
+							{' '}
+							{Object.keys(price)[0]}
+						</PriceTile>
+					))}
+				</PriceGrid>
+			)}
+		</AppContext.Consumer>
+	);
 }
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -4,6 +4,7 @@ import Page from '../shared/Page';
 import PriceGrid from './PriceGrid';
 import CoinSpotlight from './CoinSpotlight';
 import PriceChart from './PriceChart';
+import { AppContext } from '../App/AppProvider';
 
 const ChartGrid = styled.div`
 	display: grid;
@@ -12,14 +13,36 @@ const ChartGrid = styled.div`
 	grid-template-columns: 1fr 3fr;
 `;
 
+const Emptydiv = styled.div`
+	padding: 6px;
+	font-size: 23px;
+	text-align: center;
+	border: 1px inherit;
+	margin-top: 40px;
+`;
+
+function checkVisitor(firstvisit) {
+	if (firstvisit === false) {
+		return (
+			<Page name='dashboard'>
+				<PriceGrid />
+				<ChartGrid>
+					<CoinSpotlight />
+					<PriceChart />
+				</ChartGrid>
+			</Page>
+		);
+	} else {
+		return (
+			<Emptydiv> Please choose your favorite coins in settings page</Emptydiv>
+		);
+	}
+}
+
 export default function() {
 	return (
-		<Page name='dashboard'>
-			<PriceGrid />
-			<ChartGrid>
-				<CoinSpotlight />
-				<PriceChart />
-			</ChartGrid>
-		</Page>
+		<AppContext.Consumer>
+			{({ firstVisit }) => checkVisitor(firstVisit)}
+		</AppContext.Consumer>
 	);
 }
